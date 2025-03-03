c158a2eb1bc15a6ffb4a4eae3e7e6a0115a0fd9f
NaamaL <bunvin@gmail.com>
2025-03-03 10:37:43 +0200

    init SpringBoot and React

---
+++ backend/mealbudget/src/main/java/naamaantoniouk/mealbudget/MealbudgetApplication.java
@@ -0,0 +1,13 @@
+package naamaantoniouk.mealbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealbudgetApplication.class, args);
+	}
+
+}
---
+++ backend/mealbudget/src/test/java/naamaantoniouk/mealbudget/MealbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package naamaantoniouk.mealbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
